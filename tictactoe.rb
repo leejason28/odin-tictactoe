72a9c610f709c675f0a49ac5d1ee41295077e84b
leejason28 <jason928lee@gmail.com>
2023-05-09 16:34:48 -0700

    small progress. need more planning

--- tictactoe.rb
+++ tictactoe.rb
@@ -1,15 +1,37 @@
+class Game
+  attr_accessor :victory, :board
+
+  def initialize
+    @victory = false
+    @board = Board.new
+  end
+
+  def game_over?
+    
+  end
+end
+
 class Board
+  attr_reader :array
+
+  def initialize
+    @array = Array.new(3) {Array.new(3, " ")}
+  end
 end
 
 class X
   SYMBOL = 'X'
-  include Player
 end
 
 class O
   SYMBOL = 'O'
-  include Player
 end
 
-module Player
+module Playable
+  def play(row, column)
+    self.array[row][column] = SYMBOL
+  end
 end
+
+g = Game.new
+p g.board.array
